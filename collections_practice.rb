acff1ec6f51ef7fc68b31cdf83c034d308708d6b
Cristina Lugo <cmlugoce@syr.edu>
2018-06-13 02:01:01 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -12,7 +12,12 @@ end
 
 
 def sort_array_char_count(array) 
-  array.length.sort do |a, b|
-    a <=> b 
+  array.sort do |a, b|
+    if a.length == b.length
+      0 
+    elsif a.length < b.length
+      -1 
+    elsif a.length > b.length
+      1
   end 
 end 
